a3e5a525cac4b384c3d08fffc6a5cf963b6615f8
RochaLS <ld.rocha@hotmail.com>
2023-07-25 17:54:49 -0700

    create new error controller for error routing, complete 404 page

---
+++ ggwp/src/main/java/com/example/ggwp/Web/CustomErrorController.java
@@ -0,0 +1,26 @@
+package com.example.ggwp.Web;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+@AllArgsConstructor
+public class CustomErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error-404";
+            }
+        }
+        return "error-404";
+    }
+}
